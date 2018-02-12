09c1da341b594763971ecedc1c2a630080f31fdf
RomanetAdrien <adrien.romanet@etu.univ-lyon1.fr>
2018-02-12 18:00:27 -0500

    ajout dps

--- TP2_IA_Romanet_Baril/src/Node.java
+++ TP2_IA_Romanet_Baril/src/Node.java
@@ -10,7 +10,47 @@ public class Node<T> {
     private int depth;
     private int cost;
 
+    public Node getChild(int i){
+        if(children==null||children.length==0){
+            return null;
+        }
+        return children[i];
+    }
 
+    public void setNodeID(int nodeID) {
+        this.nodeID = nodeID;
+    }
 
+    public Node[] getParents() {
+        return parents;
+    }
+
+    public void setParents(Node[] parents) {
+        this.parents = parents;
+    }
+
+    public Node[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Node[] children) {
+        this.children = children;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
 
--- TP2_IA_Romanet_Baril/src/Tree.java
+++ TP2_IA_Romanet_Baril/src/Tree.java
@@ -1,6 +1,8 @@
 public class Tree<T> {
 
     private int rootNodeID;
+    private Node rootNode;
+    public Problem problem;
 
     public Tree(Node root){
         this.rootNodeID = root.getNodeID();
@@ -8,5 +10,22 @@ public class Tree<T> {
 
     public Tree(){
         this.rootNodeID = 0;
+    }
+
+
+    public int getRootNodeID() {
+        return rootNodeID;
+    }
+
+    public void setRootNodeID(int rootNodeID) {
+        this.rootNodeID = rootNodeID;
+    }
+
+    public Node getRootNode() {
+        return rootNode;
+    }
+
+    public void setRootNode(Node rootNode) {
+        this.rootNode = rootNode;
     }
 }
