8856bcfe83f32c3513fd80718c8a1906fd954170
Paul Baril <paulbaril1995@hotmail.fr>
2018-02-12 17:40:59 -0500

    Finish problem structure, modify Main, tree structure

--- TP2_IA_Romanet_Baril/src/Node.java
+++ TP2_IA_Romanet_Baril/src/Node.java
@@ -1,16 +1,29 @@
-public class Node<T> {
+import java.util.List;
+
+public class Node {
 
     private int nodeID;
     public int getNodeID() {
         return nodeID;
     }
 
-    private Node[] parents;
-    private Node[] children;
+    private List<Node> parents;
+    private List<Node> children;
     private int depth;
+    // cost to reach this node from root
     private int cost;
 
+    public Node(int nodeID) {
+        this.nodeID = nodeID;
+    }
 
+    public void AddChild(Node child){
+        children.add(child);
+    }
+
+    public void AddParent(Node parent){
+        parents.add(parent);
+    }
 
 }
 
--- TP2_IA_Romanet_Baril/src/Tree.java
+++ TP2_IA_Romanet_Baril/src/Tree.java
@@ -1,12 +1,25 @@
-public class Tree<T> {
+public class Tree {
 
-    private int rootNodeID;
-
-    public Tree(Node root){
-        this.rootNodeID = root.getNodeID();
+    private Node rootNode;
+    public Node getRootNode() {
+        return rootNode;
     }
 
-    public Tree(){
-        this.rootNodeID = 0;
+    Problem problem;
+
+    public Tree(Node rootNode, Problem problem){
+        this.rootNode = rootNode;
+        this.problem = problem;
+        CreateTree(problem,rootNode);
     }
+
+    public Node CreateTree(Problem problem, Node node){
+        for (int item: problem.S(node.getNodeID())) {
+            node.AddChild(new Node(item));
+            node.AddParent(new Node(item));
+        }
+
+        return node;
+    }
+
 }
